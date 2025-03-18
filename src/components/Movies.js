46d75b3ed2852af121ffe83eb4196fb7bde8f44e
Brian Thompson <bt268y@gmail.com>
2025-03-17 19:22:40 -0700

    Fixed movie titles visibility and updated movies.css

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,29 +1,23 @@
 import React from "react";
-import inceptionImg from '../images/inception.jpg';
-import matrixImg from '../images/matrix.jpg';
-import interstellarImg from '../images/interstellar.jpg';
-
-const movies = [
-  { id: 1, title: "Inception", image: inceptionImg },
-  { id: 2, title: "The Matrix", image: matrixImg },
-  { id: 3, title: "Interstellar", image: interstellarImg },
-];
+import "./Movies.css"; // <-- Import your CSS file here
 
 function Movies() {
+  const movies = [
+    { id: 1, title: "Inception", image: "/images/inception.jpg" },
+    { id: 2, title: "The Matrix", image: "/images/matrix.jpg" },
+    { id: 3, title: "Interstellar", image: "/images/interstellar.jpg" },
+  ];
+
   return (
     <div className="movies-container">
-      <h2>Movies List</h2>
-      <div className="movies-list">
-        {movies.map((movie) => (
-          <div key={movie.id} className="movie-card">
-            <img src={movie.image} alt={movie.title} />
-            <h3>{movie.title}</h3>
-          </div>
-        ))}
-      </div>
+      {movies.map((movie) => (
+        <div key={movie.id} className="movie">
+          <img src={movie.image} alt={movie.title} />
+          <p>{movie.title}</p>
+        </div>
+      ))}
     </div>
   );
 }
 
 export default Movies;
-S
