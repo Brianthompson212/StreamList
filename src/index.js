0f20f0c4b230cfa4894f4ead1987c0bc76ea33c0
Brian Thompson <bt268y@gmail.com>
2025-04-01 19:19:22 -0700

    Configure GitHub Pages deployment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // ✅ import this
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,5 +12,7 @@ root.render(
   </React.StrictMode>
 );
 
+// Register service worker for PWA
+serviceWorkerRegistration.register(); // ✅ call register()
 
 reportWebVitals();
